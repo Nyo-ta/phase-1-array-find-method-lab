6f617134199531e11986b8d1343c6c821caa79d5
samuel nyota karanja <samuel.karanja3@student.moringaschool.com>
2025-06-10 15:40:07 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
-function superbowlWin(record) {
-  const winresult = record.find(entry=> entry.result === "W");
-  return winresult ? winresult.year : undefined;
+function superbowlWin(myArray){
+    let superbowl = myArray.find(finder)
+    return superbowl ? superbowl.year : undefined
 }
-
-
+function finder(currentvalue){
+    let targetrecord = currentvalue.result == "W"
+    return targetrecord
+}
+console.log(superbowlWin(record));
